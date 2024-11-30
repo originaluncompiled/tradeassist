4bd9eabdb1e70719ac4bac47754be8a427f876b7
Carsten <carstendold1@gmail.com>
2024-11-30 12:16:57 +0200

    switching to development build

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,18 +2,28 @@ import { Stack } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import './global.css'
 import { SQLiteProvider } from 'expo-sqlite'
-import migrateDbIfNeeded from '@/hooks/migrateDbIfNeeded';
+import migrateDbIfNeeded from '@/hooks/migrateDbIfNeeded'
+import * as SystemUI from 'expo-system-ui'
+import { colors } from '@/constants/colors'
 
 const RootLayout = () => {
+  SystemUI.setBackgroundColorAsync(colors.dark.neutral_8);
+
   return (
     <SQLiteProvider
       databaseName="trades.db" 
       onInit={migrateDbIfNeeded}
     >
       <SafeAreaView style={{ flex: 1 }}>
-        <Stack screenOptions={{ animation: 'none' }}>
-          <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-          <Stack.Screen name='trade-history' options={{ headerShown: false }} />
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: {
+              backgroundColor: colors.dark.neutral_8 
+            }
+          }}>
+          <Stack.Screen name='(tabs)' />
+          <Stack.Screen name='trade-history' />
         </Stack>
       </SafeAreaView>
     </SQLiteProvider>
