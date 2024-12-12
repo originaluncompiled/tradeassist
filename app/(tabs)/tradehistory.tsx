2d5b741813874e2c7c8d8c5a699a4275907e177e
Carsten <carstendold1@gmail.com>
2024-12-12 16:49:45 +0200

    Splash screen shows in light/dark mode now

--- app/(tabs)/tradehistory.tsx
+++ app/(tabs)/tradehistory.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ScrollView, Text, View } from 'react-native'
 import FilterSection from '@/components/Filter/FilterSection'
 import TradeCard from '@/components/TradingHistory/TradeCard'
